cfcd66ff0180aad431584b23da5f50adffb7a79e
PanWeiBin <panweibin@weeget.cn>
2021-11-30 18:48:18 +0800

    feat: 动态引入

--- src/core/DynamicEngine.vue
+++ src/core/DynamicEngine.vue
@@ -1,23 +1,24 @@
 <template>
-	<import-component :renderItem="renderItem"></import-component>
+  <import-component :renderItem="renderItem"></import-component>
 </template>
 
 <script lang="ts" setup>
-import { defineAsyncComponent, defineProps } from 'vue'
+import { defineAsyncComponent, defineProps } from "vue";
 
-const modules = import.meta.glob('../materials/base/*/index.vue')
-const components: any = {}
-Object.keys(modules).forEach(key => {
-	const name = key.replace('../materials/base/', '').replace('/index.vue', '')
-	console.log(name, 'name')
-	components[name] = modules[key]
-})
+const modules = import.meta.glob("../materials/base/*/index.vue");
+const components: any = {};
+Object.keys(modules).forEach((key) => {
+  const name = key.replace("../materials/base/", "").replace("/index.vue", "");
+  components[name] = modules[key];
+});
 
-const { renderItem } = defineProps({
-	renderItem: Object,
-})
+const props = defineProps({
+  renderItem: Object,
+});
 
-const importComponent = defineAsyncComponent(components[renderItem?.type])
+const importComponent = defineAsyncComponent(
+  components[props.renderItem?.type]
+);
 </script>
 
 <style></style>
