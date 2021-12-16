496a3c7e85362f485b716bfc78a2047082c2f7ef
PanWeiBin <panweibin@weeget.cn>
2021-12-16 23:28:41 +0800

    feat: 预览打印

--- src/pages/Preview/index.vue
+++ src/pages/Preview/index.vue
@@ -1,18 +1,17 @@
 <template>
-  <div>
-    <DynamicEngine :renderItem="renderItem" v-for="(renderItem) in pointList" :key="renderItem.id" />
-  </div>
+  <DynamicEngine :renderItem="renderItem" v-for="(renderItem) in pointList" :key="renderItem.id" />
 </template>
 
 <script lang="ts" setup>
 import DynamicEngine from "@/core/DynamicEngine.vue";
-import { computed } from "vue";
+import { computed, toRaw } from "vue";
 import { useStore } from "vuex";
 
 const store = useStore();
 
 // 从VueX中直接取出画板数据
 const pointList = computed(() => store.state.componentArray);
+console.log(toRaw(pointList.value), "Components List");
 </script>
 
 <style>
